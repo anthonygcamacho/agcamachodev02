101f57b6e161e349836bbdf388f3b757f8d8a170
Anthony Camacho <anthony.g.camacho@gmail.com>
2023-04-19 22:33:34 -0700

    buiding

---
+++ app/src/FirebaseConfig.ts
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app"
+import { getAuth } from "firebase/auth"
+import { getFirestore } from "firebase/firestore/lite"
+import { getStorage } from "firebase/storage"
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+}
+
+const firebaseApp = initializeApp(firebaseConfig)
+const auth = getAuth(firebaseApp)
+const firestore = getFirestore(firebaseApp)
+const storage = getStorage(firebaseApp)
+
+export default {
+    auth,
+    firestore,
+    storage,
+}
